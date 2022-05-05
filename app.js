da91e2110937701718e14c2c25173c857de2f395
Ayrat Mukhametshin <airatmm@gmail.com>
2022-05-05 21:44:11 +0300

    tests endpoint

--- app.js
+++ app.js
@@ -27,12 +27,17 @@ app.get('/', (req, res) => {
 app.use('/', users);
 app.use('/', cards);
 
+app.use((req, res) => {
+  res.status(404).send('Ooops! Page not found');
+});
+
 // подключаемся к серверу mongo
 async function main() {
   await mongoose.connect('mongodb://localhost:27017/mestodb', {
     useNewUrlParser: true,
     useUnifiedTopology: false,
   });
+
   app.listen(PORT, () => {
     // Если всё работает, консоль покажет, какой порт приложение слушает
     console.log(`App listening on port ${PORT}`);
