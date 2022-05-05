4fc0368c05ad82537ecb2c5573990237575b8730
Ayrat Mukhametshin <airatmm@gmail.com>
2022-05-05 20:14:51 +0300

    tests endpoint

--- controllers/cards.js
+++ controllers/cards.js
@@ -42,6 +42,9 @@ const likeCard = async (req, res) => {
       { $addToSet: { likes: req.user._id } }, // добавить _id в массив, если его там нет
       { new: true },
     );
+    if (!req.params.cardId) {
+      res.status(400).send({ message: 'Нет карточки с таким id' });
+    }
     res.status(200).send(like);
   } catch (err) {
     res.status(500).send({ message: `На сервере произошла ошибка: ${err.message}` });
